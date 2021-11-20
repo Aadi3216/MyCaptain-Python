8e208b38b5500d30594e245bf60344a1f6799057
Aadi3216 <93978692+Aadi3216@users.noreply.github.com>
2021-11-21 03:29:42 +0530

    Add files via upload

--- project2_task2.py
+++ project2_task2.py
@@ -1,9 +1,8 @@
-range_1 =  int(input("Lower limit :"))
-range_2 =  int(input("Upper limit :"))
-for i in range(range_1,range_2+1):
-    if i%2==0:
-        print(i)
 
+list1 = [11, -21, 0, 45, 66, -93]
+for num in list1:
+    if num >= 0:
+       print(num)
 
 
 
